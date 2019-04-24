5eed54afea17a0e6362533a35745ff8f0b2222d5
sh4dow <shadoxxhd@users.noreply.github.com>
2019-04-24 17:51:04 +0200

    fix compatability with Darkstar Utilities
    see Reika_FactorioMods_Issues#151

--- prototypes/recipe/drill-recipe-updates.lua
+++ prototypes/recipe/drill-recipe-updates.lua
@@ -1,7 +1,7 @@
 if data.raw.module["green-module-5"] then
   table.insert(data.raw["recipe"]["fast-drill"].ingredients,{"green-module-5", 1})
   table.insert(data.raw["technology"]["fast-drills"].prerequisites,"green-module-5")
-elseif data.raw.module["effectivity-module-5"] then
+elseif data.raw.module["effectivity-module-5"] and data.raw.technology["effectivity-module-5"] then
   table.insert(data.raw["recipe"]["fast-drill"].ingredients,{"effectivity-module-5", 1})
   table.insert(data.raw["technology"]["fast-drills"].prerequisites,"effectivity-module-5")
 else
